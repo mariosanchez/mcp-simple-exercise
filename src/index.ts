14e0c39be79de6ae13963d2d327d46fe5043cfcc
Mario Sánchez <mario.sanchez@sunwebgroup.com>
2025-07-01 13:00:31 +0200

    create main function to run server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import {StdioClientTransport} from "@modelcontextprotocol/sdk/client/stdio.js";
+import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
 import {setUpServer} from "./server.js";
 
 const server = setUpServer({})
+
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Weather MCP Server running on stdio");
+}
+
+main().catch((error) => {
+    console.error("Fatal error:", error)
+    process.exit(1)
+})
