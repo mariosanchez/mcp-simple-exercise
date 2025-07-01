fea5528cdf2dc02595aa95f1ec5ac8dd21184a0b
Mario Sánchez <mario.sanchez@sunwebgroup.com>
2025-07-01 10:56:59 +0200

    define types for weather data client

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export type Alert = {
+    event: string
+    areaDescription: string
+    severity: string
+    status: string
+    headline: string
+}
+
+export type ForecastPeriod = {
+    name: string
+    temperature: number
+    temperatureUnit: string
+    windSpeed: string
+    windDirection: string
+    shortForecast: string
+}
+
+export type GetAlertsResponse = {
+    data: Alert[]
+}
+
+export type GetForecastResponse = {
+    data: ForecastPeriod[]
+}
+
+export interface WeatherDataClient { 
+    getAlerts: () => GetAlertsResponse
+    getForecast: () => GetForecastResponse
+}
